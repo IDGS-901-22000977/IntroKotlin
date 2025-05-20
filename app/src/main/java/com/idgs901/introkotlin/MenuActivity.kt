d8b62307a8ee7f740a40c6a223bffa5139daf2a5
IDGS-801-22000977 <79349@alumnos.utleon.edu.mx>
2025-05-19 23:41:33 -0600

    Activitys 19/05/2025

---
+++ app/src/main/java/com/idgs901/introkotlin/MenuActivity.kt
@@ -0,0 +1,33 @@
+package com.idgs901.introkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.idgs901.introkotlin.ejemplo1.SumaActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        val btnSaludo = findViewById<Button>(R.id.btn1)
+
+        btnSaludo.setOnClickListener {navegateToSaludo()}
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun navegateToSaludo(){
+        val intent = Intent(this, SumaActivity::class.java)
+        startActivity(intent)
+    }
+}
